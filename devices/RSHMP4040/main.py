6bc51253802475e5ae636c06195ca79ea645e6cc
Mark Wong <mark@squarepotato.com>
2022-10-28 18:41:00 +0200

    add off state

--- devices/RSHMP4040/main.py
+++ devices/RSHMP4040/main.py
@@ -29,6 +29,12 @@ def on():
 def off():
     instr = RsInstrument("TCPIP::172.16.32.113::5025::SOCKET", id_query=True, reset=True)
     instr.write_str('OUTP:GEN 0')
+    instr.write_str('INST OUT3')
+    ch3 = instr.query_str('OUTP?')
+    instr.write_str('INST OUT4')
+    ch4 = instr.query_str('OUTP?')
+    print(f"channel3 state: {ch3}, channel4 state: {ch4}")
+    instr.close()
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("usage: python3 main.py [on|off]")
