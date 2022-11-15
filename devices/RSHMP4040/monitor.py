3bbe79e22df4df51bc8c8bc3c39424e7202e6109
Mark Wong <mark@squarepotato.com>
2022-11-15 18:56:12 +0100

    send measured current and voltage via zeromq

---
+++ devices/RSHMP4040/monitor.py
@@ -0,0 +1,24 @@
+from RsInstrument import *
+import zmq, time, math
+def monitor():
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://172.16.32.214:2001")
+    counter = 0
+    try:
+        while True:
+            instr = RsInstrument("TCPIP::172.16.32.113::5025::SOCKET", id_query=True, reset=False)
+            instr.write_str('INST OUT3')
+            ch3_c = instr.query_str('MEAS:CURR?')
+            ch3_v = instr.query_str('MEAS:VOLT?')
+            instr.write_str('INST OUT4')
+            ch4_c = instr.query_str('MEAS:CURR?')
+            ch4_v = instr.query_str('MEAS:VOLT?')
+            instr.close()
+            data = {'timestamp': math.floor(time.time() ), 'ch3_c': float(ch3_c), 'ch3_v': float(ch3_v), 'ch4_c': float(ch4_c), 'ch4_v': float(ch4_v) }
+            socket.send_json(data)
+            time.sleep(5) #5 seconds
+    except KeyboardInterrupt:
+        pass
+if __name__ == '__main__':
+    monitor()
