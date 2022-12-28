bc8f746ce04b52dabd9965ec68ae077ff6f5fa67
Mark Wong <mark@squarepotato.com>
2022-12-28 18:17:15 +0100

    catch NoneType exception

--- devices/DS1490F/monitor.py
+++ devices/DS1490F/monitor.py
@@ -12,7 +12,10 @@ def monitor():
             with Onewire('u') as o:
                 r['timestamp'] = time.time()
                 for s in o.find(has_all=['temperature']):
-                    r[f"{s.family}.{s.id}"] = s.read_float('temperature')
+                    try:
+                        r[f"{s.family}.{s.id}"] = s.read_float('temperature')
+                    except TypeError:
+                        continue
             socket.send_json(r)
             time.sleep(5) # 5 seconds
     except KeyboardInterrupt:
